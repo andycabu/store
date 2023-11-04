2c64866fa011d8ff5cbed240a293c3b5707814a2
andycabu-PC <andycabu@gmail.com>
2023-11-04 22:39:48 +0100

    arreglos en boton modo oscuro

--- src/components/DayNight.jsx
+++ src/components/DayNight.jsx
@@ -1,36 +1,25 @@
-import { useEffect, useState } from "react";
 import PropTypes from "prop-types";
+import { useTheme } from "../hooks/useTheme";
 
 function DayNight({ position }) {
-  const [theme, setTheme] = useState(() => {
-    const savedTheme = localStorage.getItem("theme");
-    if (savedTheme) {
-      return savedTheme;
-    } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-      return "dark";
-    }
-    return "light";
-  });
+  const { changeTheme, theme } = useTheme();
 
-  useEffect(() => {
-    const root = window.document.documentElement;
-    root.classList.remove("light", "dark");
-    root.classList.add(theme);
-
-    localStorage.setItem("theme", theme);
-  }, [theme]);
-
-  function changeTheme() {
-    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
-  }
+  const handleToggle = () => {
+    changeTheme(); //
+  };
 
   return (
     <label className={`day-night z-10 ${position}`}>
-      <input onClick={changeTheme} type="checkbox" checked={theme === "dark"} />
+      <input
+        onChange={handleToggle}
+        type="checkbox"
+        checked={theme === "dark"}
+      />
       <div></div>
     </label>
   );
 }
+
 DayNight.propTypes = {
   position: PropTypes.string,
 };
