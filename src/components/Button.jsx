242971581f1266267bc80969b297448938bf1f94
andycabu <andycabu@gmail.com>
2023-11-16 17:11:26 +0100

    autentificaciones

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,12 @@
 import PropTypes from "prop-types";
 
-const Button = ({ onClick, background, text, icon }) => {
+const Button = ({ onClick, background, text, icon, textColor }) => {
   return (
     <button
       onClick={onClick}
       className={`rounded py-2 px-4  flex items-center justify-center gap-4  transition-all hover:scale-105 duration-300 ${
         background ? background : "bg-blue-500 hover:bg-blue-600 "
-      } text-white`}
+      } ${textColor ? textColor : "text-white"} `}
     >
       {text} {icon}
     </button>
@@ -17,6 +17,7 @@ Button.propTypes = {
   onClick: PropTypes.func,
   background: PropTypes.string,
   text: PropTypes.string,
+  textColor: PropTypes.string,
   icon: PropTypes.any,
 };
 export default Button;
